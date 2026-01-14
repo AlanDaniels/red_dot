8e2cc4c99172c362d94e45ecc68d40cfbb670c1f
Alan Daniels <alan@alandaniels.com>
2026-01-14 18:28:59 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "raylib.h"
+
+int main(void)
+{
+    constexpr int SCREEN_WIDTH = 800;
+    constexpr int SCREEN_HEIGHT = 450;
+
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib [core] example - basic window");
+    SetTargetFPS(60);
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
