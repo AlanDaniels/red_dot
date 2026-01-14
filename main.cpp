3c18e54986991dfdfb84d0669f4b4d92e833ee18
Alan Daniels <alan@alandaniels.com>
2026-01-14 18:47:27 -0500

    Moving the dot around, so exciting

--- main.cpp
+++ main.cpp
@@ -8,14 +8,23 @@ int main(void)
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib [core] example - basic window");
     SetTargetFPS(60);
 
+    Vector2 ballPosition = { SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT / 2.0f };
+
     InitAudioDevice();
     Sound another = LoadSound("../Another World.mp3");
     PlaySound(another);
 
     while (!WindowShouldClose()) {
+
+        if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 2.0f;
+        if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 2.0f;
+        if (IsKeyDown(KEY_UP)) ballPosition.y -= 2.0f;
+        if (IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
+        DrawCircleV(ballPosition, 50, MAROON);
         EndDrawing();
     }
 
