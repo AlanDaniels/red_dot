c0f0af4651d36428c20fe902405a8d3f8be1fa93
Alan Daniels <alan@alandaniels.com>
2026-01-21 19:12:36 -0500

    The dot collection renders correctly now

--- constants.hpp
+++ constants.hpp
@@ -5,8 +5,10 @@ constexpr int SCREEN_WIDTH = 1024;
 constexpr int SCREEN_HEIGHT = 1024;
 constexpr int FONT_BASE_SIZE = 30;
 
-constexpr int DOT_GRID_ROWS = 4;
-constexpr int DOT_GRID_COLS = 4;
+constexpr int DOT_GRID_ROWS = 5;
+constexpr int DOT_GRID_COLS = 5;
+
+constexpr int DOT_SPACING = 150;
 constexpr int DOT_RADIUS = 50;
 
 #endif
--- dot_collection.cpp
+++ dot_collection.cpp
@@ -2,13 +2,23 @@
 
 
 // Constructor.
+// TODO: Our offset logic only works with an odd number of rows or columns. Fix this later.
 void DotCollection::init()
 {
+    int x_offset = DOT_SPACING * (DOT_GRID_ROWS / 2);
+    int x = (SCREEN_WIDTH / 2) - x_offset;
+
     for (int r = 0; r < DOT_GRID_ROWS; r++) {
+        int y_offset = DOT_SPACING * (DOT_GRID_COLS / 2);
+        int y = (SCREEN_HEIGHT / 2) - y_offset;
+
         for (int c = 0; c < DOT_GRID_COLS; c++) {
             m_dots[r][c].setColor(RED);
-            m_dots[r][c].setPos(500, 500);
+            m_dots[r][c].setPos(x, y);
+            y += DOT_SPACING;
         }
+
+        x += DOT_SPACING;
     }
 }
 
@@ -16,6 +26,7 @@ void DotCollection::init()
 void DotCollection::render() const
 {
     for (int r = 0; r < DOT_GRID_ROWS; r++) {
+
         for (int c = 0; c < DOT_GRID_COLS; c++) {
             Color color = m_dots[r][c].getColor();
             Vector2 pos = m_dots[r][c].getPos();
